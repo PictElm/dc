0db0c45d37214ecc2afff82b8adbfbf84cc26357
Grenier Celestin <celestin.grenier@hotmail.com>
2022-09-16 15:29:34 +0200

    as expected, it looks wrong...

--- Main.hs
+++ Main.hs
@@ -30,9 +30,9 @@ mapBothSndBin = (.) mapBoth . flip (mapSnd . (|$))
 mayHead :: [a] -> Maybe a
 mayHead = foldr (const . Just) Nothing
 
-mayIfStarts :: Char -> Input -> Maybe Input
-mayIfStarts = (|.) duple . (.) (uncurry fmap)
-            . mapBothSndBin (const , (mayHead .) . takeWhile . (==))
+mayIfHead :: (Char -> Bool) -> Input -> Maybe Input
+mayIfHead = (. duple) . (.) (uncurry fmap)
+            . mapBothSndBin (const, (mayHead .) . takeWhile)
 
 sure :: Maybe a -> a
 sure = maybe undefined id
@@ -47,111 +47,170 @@ type Stack = [Item]
 -- type Registers = Map Char (Stack, [Item])
 
 type State = (Stack)
+-- type MacroStack = [Action]
+-- type State = (Stack, RegisterMap, MacroStack)
 
 type Action = State -> State
 
---- parser
+exec :: [Action] -> Action
+exec = foldl1 (|.)
+
+--- lexers/scanners/parsers
 data Token -- YYY: will probably Token = Action soon
   = Val Item
   | Op String --Action
   deriving (Show, Eq)
 
 type Lexer = Input -> Maybe (Token, Rest)
-
-{-
-script ::= {action}
-action ::= op | val
-
-val ::= num | str
-num ::= /[0-9A-Z]+/
-str ::= /\[.*\]/
-
-op ::=
-  | /[pnPf]/       -- printing commands
-  | /[+\-*/%~^|v]/ -- arithmetic
-  | /[cdrR]/       -- stack control
-  | /[slSL]\w/     -- registers
-  | /[iokIOK]/     -- parameters
-  | /[ax]/         -- strings (& macros)
-  | /!?[<=>]\w/
-  | /[\?qQ]/
-  | /[ZXz]/        -- statud inquiry
-  | /[!#].*\n/     -- miscellianeous
-  | /[:;]\w/       -- no (or not yet)
-
-
-simpleOp   ::=   x
-complexOp  ::=   x r
-complexOp2 ::= y x r
-longOp     ::= b c*
-delimOp is longOp where c is .\e
-
-
-longOp ::= pb cb
--}
+type SureLexer = Input -> (Token, Rest)
 
 type Accepts = [Char]
 type Input = String
-type Output = String
+type Output = [Char]
 type Rest = String
 
-lexnt :: Lexer
-lexnt = const (Just (Op "nop", ""))
-
+-- | mkLexSimple x
+--   parses `it ::= x`
 mkLexSimple :: Char -> Token -> Lexer
-mkLexSimple x t = mkLexLong (==x) (const False) (const t)
+mkLexSimple = (. const) . flip mkLexLong (const False) . (==)
 
-mkSureLexComplex :: (Char -> Token) -> Input -> (Token, Rest)
-mkSureLexComplex = (|.) duple
-                 . mapBothFstBin ((. head . drop 1) , drop 2)
+mkSureLexComplex :: (Char -> Token) -> SureLexer
+mkSureLexComplex = (. duple)
+                 . mapBothFstBin ((. head . drop 1), drop 2)
+-- YYY: at head: "error: expected register next"
+--      but this is not a solution (to crash); it
+--      should just have been Nothing on the level
+--      of the may lexer
 
+-- | mkLexComplex x (f r)
+--   parses `it ::= x <r>`
+--   and r will be any character
 mkLexComplex :: Char -> (Char -> Token) -> Lexer
--- mkLexComplex x fr = \i ->
---                       if x == head i
---                         then Just (fr (head (drop 1 i)), drop 2 i)
---                         else Nothing
--- mkLexComplex x fr = (fmap . mkSureLexComplex) fr . (mayIfStarts x)
-mkLexComplex = (|.) (fmap . mkSureLexComplex) . (|.) . mayIfStarts
+mkLexComplex = (. fmap . mkSureLexComplex) . (|.) . mayIfHead . (==)
 
+-- | mkLexComplex2 y x (f r)
+--   parses `it ::= y x <r>`
+--   and r will be any character
 mkLexComplex2 :: Char -> Char -> (Char -> Token) -> Lexer
--- mkLexComplex2 y x fr = \i ->
---                          if y == head i
---                            then (mkLexComplex x fr) (drop 1 i)
---                            else Nothing
--- mkLexComplex2 y x fr = \i -> fmap (sure . mkLexComplex x fr . drop 1) (mayIfStarts y i)
--- mkLexComplex2 = (|.) mkLexComplex . (.) . (|.) (fmap . (.) sure . (drop 1 |.)) . (|.) . mayIfStarts
-mkLexComplex2 = (mkLexComplex |.) . (.) . (. (=<<)) . (|.) . mayIfStarts -- crap, missed the `drop 1`
+mkLexComplex2 = (. mkLexComplex) . (.) . (. (=<<)) . (|.) . (.) (drop 1 <$>) . mayIfHead . (==)
 
-mkLexLong :: (Char -> Bool) -> (Char -> Bool) -> ([Char] -> Token) -> Lexer
-mkLexLong pb pc fc = \i ->
-                       if pb (head i) -- fmap (\c -> ..) . mayHead
-                         then
-                           let
-                             c = head i
-                             (o, r) = span pc (drop 1 i)
-                           in Just (fc (c:o), r)
-                         else Nothing
+mkSureLexLong :: (Char -> Bool) -> (Output -> Token) -> SureLexer
+mkSureLexLong = (. f) . g
+              where
+                f = (uncurry . mapBothFstBin)
+                  . (flip (,)) id . (. (:) . head) . (.)
+                g = (|.) . (. duple) . mapSnd . (. drop 1) . span
 
+-- | mkLexLong pb pc (f c)
+--   parses `it ::= ?pb {?pc}`
+--   where pb and pc are predicate begin and continue
+mkLexLong :: (Char -> Bool) -> (Char -> Bool) -> (Output -> Token) -> Lexer
+mkLexLong = (. mkSureLexLong) . (.) . (. fmap) . (|.) . mayIfHead
 
+lexNum = (mkLexLong $$) (flip elem $ "1-9A-Z_") (Val . Num . read)
+lexStr = mkLexLong (=='[') (/=']') (Val . Str . tail)
+lexSkip = (mkLexLong $$) (flip elem $ "\t\n\r ]") (const (Op "nop"))
+-- printing commands
+lexPln = mkLexSimple 'p' (Op "pln")
+lexNln = mkLexSimple 'n' (Op "nln")
+lexPnt = mkLexSimple 'P' (Op "pnt")
+lexDmp = mkLexSimple 'f' (Op "dmp")
+-- arithmetic
 lexAdd = mkLexSimple '+' (Op "add")
 lexSub = mkLexSimple '-' (Op "sub")
-
-lexLt = mkLexComplex '<' (Op . ((flip (:)) ": ifLt (exec . getR)"))
-lexNLt = mkLexComplex2 '!' '<' (Op . ((flip (:)) ": ifGt (exec . getR)"))
-
-lexNum = (($$) mkLexLong) (flip elem $ "1-9A-Z_") (Val . Num . read)
-lexStr = mkLexLong (=='[') (/=']') (Val . Str . tail)
--- YYY: hence needs to discard the ']' (see `lexSkip`)
-
+lexMul = mkLexSimple '*' (Op "mul")
+lexDiv = mkLexSimple '/' (Op "div")
+lexRem = mkLexSimple '%' (Op "rem")
+lexQuo = mkLexSimple '~' (Op "quo")
+lexExp = mkLexSimple '^' (Op "exp")
+lexMex = mkLexSimple '|' (Op "mex")
+lexSrt = mkLexSimple 'v' (Op "srt")
+-- stack control
+lexClr = mkLexSimple 'c' (Op "clr")
+lexDup = mkLexSimple 'd' (Op "dup")
+lexSwp = mkLexSimple 'r' (Op "swp")
+lexRot = mkLexSimple 'R' (Op "rot")
+-- registers
+lexSer = mkLexComplex 's' (Op . ((flip (:)) "#ser"))
+lexGer = mkLexComplex 'l' (Op . ((flip (:)) "#ger"))
+lexPur = mkLexComplex 'S' (Op . ((flip (:)) "#pur"))
+lexPor = mkLexComplex 'L' (Op . ((flip (:)) "#por"))
+-- parameters
+lexSir = mkLexSimple 'i' (Op "sir")
+lexSor = mkLexSimple 'o' (Op "sor")
+lexSpr = mkLexSimple 'k' (Op "spr")
+lexGir = mkLexSimple 'I' (Op "gir")
+lexGor = mkLexSimple 'O' (Op "gor")
+lexGpr = mkLexSimple 'K' (Op "gpr")
+-- strings (& macros)
+lexBla = mkLexSimple 'a' (Op "bla")
+lexExc = mkLexSimple 'x' (Op "exc")
+lexGt = mkLexComplex '>' (Op . ((flip (:)) "#gt"))
+lexNGt = mkLexComplex2 '!' '>' (Op . ((flip (:)) "#le"))
+lexLt = mkLexComplex '<' (Op . ((flip (:)) "#lt"))
+lexNLt = mkLexComplex2 '!' '<' (Op . ((flip (:)) "#ge"))
+lexEq = mkLexComplex '=' (Op . ((flip (:)) "#eq"))
+lexNEq = mkLexComplex2 '!' '=' (Op . ((flip (:)) "#ne"))
+lexRdx = mkLexSimple '?' (Op "rdx")
+lexQui = mkLexSimple 'q' (Op "qui")
+lexMqu = mkLexSimple 'Q' (Op "mqu")
+-- status inquiry
+lexNdd = mkLexSimple 'Z' (Op "ndd")
+lexNfd = mkLexSimple 'X' (Op "nfd")
+lexDpt = mkLexSimple 'z' (Op "dpt")
+-- miscellianeous
 lexCmd = mkLexLong (=='!') (/='\n') (const (Op "sh . drop first"))
 lexCmt = mkLexLong (=='#') (/='\n') (const (Op "nop"))
-
--- lexSkip = (($$) mkLexLong) (isBlank or ']') (const (Op "nop"))
+--lexEq = mkLexComplex ':' (Op . ((flip (:)) "#set"))
+--lexEq = mkLexComplex ';' (Op . ((flip (:)) "#get"))
 
 lexers =
   [ lexNum
+  , lexStr
+  , lexSkip
+  , lexPln
+  , lexNln
+  , lexPnt
+  , lexDmp
   , lexAdd
   , lexSub
+  , lexMul
+  , lexDiv
+  , lexRem
+  , lexQuo
+  , lexExp
+  , lexMex
+  , lexSrt
+  , lexClr
+  , lexDup
+  , lexSwp
+  , lexRot
+  , lexSer
+  , lexGer
+  , lexPur
+  , lexPor
+  , lexSir
+  , lexSor
+  , lexSpr
+  , lexGir
+  , lexGor
+  , lexGpr
+  , lexBla
+  , lexExc
+  , lexGt
+  , lexNGt
+  , lexLt
+  , lexNLt
+  , lexEq
+  , lexNEq
+  , lexRdx
+  , lexQui
+  , lexMqu
+  , lexNdd
+  , lexNfd
+  , lexDpt
+  , lexCmd
+  , lexCmt
   ]
 
 next :: Input -> (Token, Rest)
@@ -172,9 +231,6 @@ parse = map (does . fst . fst)
       . iterate (next . snd)
       . (,) undefined
       where zip1Off = uncurry (zip . drop 1) . duple
-
-exec :: [Action] -> Action
-exec = foldl1 (|.)
 
 --- entry point
 --main and such
